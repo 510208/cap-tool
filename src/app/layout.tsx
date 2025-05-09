eb47c88c348d083c4938937cbf851197a3424720
SamHacker <xux510208@gmail.com>
2025-05-09 21:45:52 +0800

    fix: 修復錯誤的逗號

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GTM_ID = "GTM-WQ48K6Q5"; // 替換成你的 GTM ID
+
   return (
     <html lang="en">
       <head>
@@ -33,11 +35,11 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w，d，s，l，i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              (function(w, d, s, l, i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j，f);
-              })(window，document,'script','dataLayer','${GTM_ID}');
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j, f);
+              })(window, document, 'script', 'dataLayer', '${GTM_ID}');
             `,
           }}
         />
@@ -45,10 +47,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQ48K6Q5"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
